79a06a7f7f22199123f90d008add49cd6efd76a2
SheldonAI <mukhint33@gmail.com>
2024-11-14 15:19:59 +0500

    The main app is ready, pages index nad about

--- main/views.py
+++ main/views.py
@@ -4,13 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context: dict = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME',
-        'list': ['first', 'second'], 
-        'dict': {'first': 1},
-        'is_authenticated': True
+        'title': 'Home - Главная',
+        'content': "Магазин мебели HOME"
+        
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context: dict = {
+        'title': 'Home - О нас',
+        'content': "О нас", 
+        'text_on_page': 'Wise mans say would it be a sin if cant'
+        
+    }
+    return render(request, 'main/about.html', context)
