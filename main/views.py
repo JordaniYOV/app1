e4314665afe5e0c040ff7f1ee0f31d2bade817a6
SheldonAI <mukhint33@gmail.com>
2025-02-19 21:50:14 +0900

    class based views

--- main/views.py
+++ main/views.py
@@ -1,22 +1,47 @@
-from django.http import HttpResponse
+from re import template
+from typing import Any
+from webbrowser import get
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Categories
 
-# Create your views here.
-def index(request):
 
-    context: dict = {
-        'title': 'Home - Главная',
-        'content': "Магазин мебели HOME",     
-    }
-    return render(request, 'main/index.html', context)
 
-def about(request):
-    context: dict = {
-        'title': 'Home - О нас',
-        'content': "О нас", 
-        'text_on_page': 'Wise mans say would it be a sin if cant'
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Home - Главная' 
+        context['content'] = 'Магазин мебели Home'
+        return context
+    
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Home - О нас' 
+        context["content"] = 'О нас'
+        context['text_on_page'] = "Cooll magazin"
+        return context
+    
+
+
+# def index(request):
+
+#     context: dict = {
+#         'title': 'Home - Главная',
+#         'content': "Магазин мебели HOME",     
+#     }
+#     return render(request, 'main/index.html', context)
+
+# def about(request):
+#     context: dict = {
+#         'title': 'Home - О нас',
+#         'content': "О нас", 
+#         'text_on_page': 'Wise mans say would it be a sin if cant'
         
-    }
-    return render(request, 'main/about.html', context)
+#     }
+#     return render(request, 'main/about.html', context)
